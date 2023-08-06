60bb695366dfcebc61641aae7c0cdc13ebd9a65a
Paul Cairns <paulcair@gmail.com>
2023-08-06 12:34:57 -0400

    Initial REST API for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -22,7 +22,10 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message : 'Set goals'}) 
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal) 
 })
 
 // @desc Update goal
@@ -30,16 +33,37 @@ const setGoal = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message : `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
 
-const deleteGoal = async (req, res) => {
-    res.status(200).json({message : `Delete goal ${req.params.id}`})
-}
+const deleteGoal = asyncHandler(async (req, res) => {
+    
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await Goal.findByIdAndRemove(req.params.id)
+
+    res.status(200).json({ id: req.params.id })
+})
 
 module.exports = {
     getGoals, setGoal, updateGoal, deleteGoal
