dfd5687142f1aa21e7b35fcfb8f94e1820aca000
Paul Cairns <paulcair@gmail.com>
2023-08-09 11:48:50 -0400

    Added the Browser Router functionality, and header icoons. Also added the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Header from './components/Header'
+
 
 
 function App() {
