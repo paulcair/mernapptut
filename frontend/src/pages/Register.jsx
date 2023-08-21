38b3faa6ed1c39b2499749540c0dd16cf94bc1e9
Paul Cairns <paulcair@gmail.com>
2023-08-21 12:13:09 -0400

    Added the register functionality and tested that it is sent to localStorage. Everything works.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,6 +3,8 @@ import {useSelector, useDispatch} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
+import {register, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -14,6 +16,26 @@ function Register() {
 
   const { name, email, password, password2 } = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSucces, message} = useSelector(
+    (state)=> state.auth
+    )
+
+  useEffect(()=>{
+    if(isError){
+      toast.error(message)
+    }
+
+    if(isSucces || user){
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isError, isSucces, message, navigate, dispatch])
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -23,6 +45,22 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    if (password !== password2){
+      toast.error('Passwords do not match')
+    }else{
+      const userData = {
+        name, 
+        email, 
+        password,
+      }
+
+      dispatch(register(userData))
+    }
+  }
+
+  if(isLoading){
+    return <Spinner/>
   }
 
   return (<>
